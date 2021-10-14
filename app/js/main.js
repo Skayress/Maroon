1067d9de18c77992be3c993dbef1dbfe5b22db9b
Skayress <demangroshev@yandex.ru>
2021-10-14 23:27:36 +0400

    create: added responsive section header and about

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 $(function(){
-	
+	// МЕНЮ БУРГЕР
+	const burger = document.getElementById('burger');
+	const header = document.getElementById('header');
+	const menu = document.getElementById('header-menu');
+
+	const toggleClass = () => {
+		header.classList.toggle('header--transparent');
+		burger.classList.toggle('burger--active');
+		menu.classList.toggle('menu--active');
+	}
+
+	burger.addEventListener('click', toggleClass);
+
+	// СКРОЛЛ ХЕДЕРА
+	$(window).scroll(function(){
+		let scrolled = $(this).scrollTop();
+		if( scrolled >= 30 ) {
+			$('.header').removeClass('header--transparent');
+		}   
+		if( scrolled <= 30 ) {     
+			$('.header').addClass('header--transparent');
+		}
+  });
 });
